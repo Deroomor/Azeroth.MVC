17c23fa3263436a6f82e0190ffeb9b1c8ce4fa6d
eeroom <eedroom@163.com>
2018-06-18 10:15:22 +0800

    对cshtml和aspx视图页进行重定向，便于调试

--- T4.Web/Global.asax.cs
+++ T4.Web/Global.asax.cs
@@ -15,6 +15,18 @@ namespace T4.Web
             System.Web.Routing.RouteTable.Routes.Add(new System.Web.Routing.Route("{controller}/{action}",new Azeroth.Klz.RouteHandler()));
         }
 
-        
+        public override void Init()
+        {
+            this.BeginRequest += Global_BeginRequest;
+        }
+
+        void Global_BeginRequest(object sender, EventArgs e)
+        {
+            string tmp=this.Request.Url.AbsolutePath.ToLower();
+            if (tmp.EndsWith(".cshtml"))
+                this.Response.Redirect(tmp.Substring(0,tmp.Length-7));
+            if (tmp.EndsWith(".aspx"))
+                this.Response.Redirect(tmp.Substring(0, tmp.Length - 4));
+        }
     }
 }
